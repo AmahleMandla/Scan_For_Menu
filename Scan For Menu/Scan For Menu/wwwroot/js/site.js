f2bfb7b7e5b86ef77e2a37368f96b20b7b002106
Amahle <Amahleaniitta@gmail.com>
2021-10-26 19:40:19 +0200

    Redid the Menu

--- Scan For Menu/Scan For Menu/wwwroot/js/site.js
+++ Scan For Menu/Scan For Menu/wwwroot/js/site.js
@@ -159,10 +159,19 @@ function EnableBtn(cname, ccnum, expmonth, cvv) {
     }
 }
 
-
-
-
-
+function hourglass() {
+    var a;
+    a = document.getElementById("div1");
+    a.innerHTML = "&#xf251;";
+    setTimeout(function () {
+        a.innerHTML = "&#xf252;";
+    }, 1000);
+    setTimeout(function () {
+        a.innerHTML = "&#xf253;";
+    }, 2000);
+}
+hourglass();
+setInterval(hourglass, 3000);
 
 
 
